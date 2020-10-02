ca52074eed2f11b05bfa7e707d238694a5ce569d
h2-heo <h0h6h2h5@gmail.com>
2020-10-02 13:51:06 +0900

    Rename fields

--- app/src/main/java/com/example/signupsample/model/User.kt
+++ app/src/main/java/com/example/signupsample/model/User.kt
@@ -3,12 +3,12 @@ package com.example.signupsample.model
 import java.util.*
 
 data class User(
-    val email: String,
+    val emailAddress: String,
     val password: String,
     val nickname: String,
     val birthDate: Date,
     val gender: Gender,
-    val hasAcceptedTerms: Boolean,
-    val hasAcceptedMarketingTerms: Boolean
+    val hasAgreedToTerms: Boolean,
+    val hasAgreedToMarketingTerms: Boolean
 ) {
 }
